90fd9193fbd2e5379793818e73cae9873932a5d6
Mikael Grankvist <mgrankvi@vaadin.com>
2025-01-30 12:57:31 +0200

    Add SwitchUserFilter

--- src/main/java/com/example/application/security/SecurityConfig.java
+++ src/main/java/com/example/application/security/SecurityConfig.java
@@ -3,15 +3,23 @@ package com.example.application.security;
 import java.util.Collections;
 
 import com.example.application.views.LoginView;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.switchuser.SwitchUserFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.vaadin.flow.spring.security.VaadinAwareSecurityContextHolderStrategy;
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -28,10 +36,28 @@ public class SecurityConfig extends VaadinWebSecurity {
     }
   }
 
+  @Autowired
+  private ApplicationContext applicationContext;
+
+  @Bean
+  public SwitchUserFilter switchUserFilter() {
+    SwitchUserFilter filter = new SwitchUserFilter();
+    filter.setSecurityContextHolderStrategy(
+            applicationContext.getBean(VaadinAwareSecurityContextHolderStrategy.class));
+    filter.setUserDetailsService(userDetailsService());
+    filter.setSwitchUserMatcher(antMatcher(HttpMethod.GET, "/impersonate"));
+    filter.setSwitchFailureUrl("/switchUser");
+    filter.setExitUserMatcher(antMatcher(HttpMethod.GET, "/impersonate/exit"));
+    filter.setTargetUrl("/");
+    return filter;
+  }
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     // Authorize access to /images/ without authentication
-    http.authorizeRequests().antMatchers("/images/**").permitAll();
+    http.authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/images/**")).permitAll());
+    http.authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/switchUser")).hasAnyRole("ADMIN", "PREVIOUS_ADMINISTRATOR"));
+    http.authorizeHttpRequests(auth -> auth.requestMatchers(new AntPathRequestMatcher("/impersonate/exit")).hasRole("PREVIOUS_ADMINISTRATOR"));
 
     // Set default security policy that permits Vaadin internal requests and
     // denies all other
--- src/main/java/com/example/application/views/MainLayout.java
+++ src/main/java/com/example/application/views/MainLayout.java
@@ -75,27 +75,19 @@ public class MainLayout extends AppLayout {
         updateUserName(SecurityService.getAuthenticatedUser());
 
         Button impersonate = new Button("Impersonate user", e -> {
-            UserDetails user = userDetailsService.loadUserByUsername("user");
-            impersonate(user, (VaadinServletRequest) VaadinRequest.getCurrent());
+            getUI().ifPresent(ui -> ui.getPage().setLocation("/impersonate?username=user"));
         });
 
         impersonate.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
         impersonate.setIcon(VaadinIcon.USER.create());
 
-        Button impersonateAnonymous = new Button("Impersonate anonymous", e -> {
-            UserDetails anonymous = SecurityService.getAnonymousUser();
-            impersonate(anonymous, (VaadinServletRequest) VaadinRequest.getCurrent());
-        });
-
-        impersonateAnonymous.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
-        impersonateAnonymous.setIcon(VaadinIcon.QUESTION.create());
 
         Button exitImpersonation = new Button("Exit impersonation", e ->
                 exitImpersonated());
 
         exitImpersonation.addThemeVariants(ButtonVariant.LUMO_ERROR);
 
-        header.add(impersonate, impersonateAnonymous, exitImpersonation);
+        header.add(impersonate, exitImpersonation);
 
 
         addToNavbar(header);
@@ -121,84 +113,10 @@ public class MainLayout extends AppLayout {
         }
     }
 
-    private void impersonate(UserDetails targetUser, VaadinServletRequest request) {
-        UsernamePasswordAuthenticationToken targetUserRequest =
-                createSwitchUserToken(request.getHttpServletRequest(), targetUser);
-        // TODO: Before switching, a check should be made on whether the user is already impersonated.
-        SecurityContext context = this.securityContextHolderStrategy.createEmptyContext();
-        context.setAuthentication(targetUserRequest);
-        this.securityContextHolderStrategy.setContext(context);
-        // TODO: redirect, context persistence and other stuff should be done in an impersonation listener
-        UI.getCurrent().getPage().setLocation("/list");
-    }
-
     private void exitImpersonated() {
-        Authentication current = this.securityContextHolderStrategy.getContext().getAuthentication();
-        Authentication original = getOriginalAuthentication(current).orElseThrow(
-                () -> new RuntimeException("Impersonated anonymous user"));
-        SecurityContext context = this.securityContextHolderStrategy.createEmptyContext();
-        context.setAuthentication(original);
-        this.securityContextHolderStrategy.setContext(context);
-        // TODO: redirect, context persistence and other stuff should be done in an impersonation listener
-        UI.getCurrent().getPage().setLocation("/");
+        getUI().ifPresent(ui -> ui.getPage().setLocation("/impersonate/exit"));
     }
 
-    private UsernamePasswordAuthenticationToken createSwitchUserToken(HttpServletRequest request,
-                                                                      UserDetails targetUser) {
-        List<GrantedAuthority> switchUserAuthoritiesList = getMergedGrantedAuthorities(targetUser);
-        UsernamePasswordAuthenticationToken targetUserRequest =
-                UsernamePasswordAuthenticationToken.authenticated(targetUser, targetUser.getPassword(),
-                        switchUserAuthoritiesList);
-        WebAuthenticationDetailsSource webAuthenticationDetailsSource = new WebAuthenticationDetailsSource();
-        targetUserRequest.setDetails(webAuthenticationDetailsSource.buildDetails(request));
-        return targetUserRequest;
-    }
-
-    /**
-     * Merges the granted authorities of the target user with the current user's granted authorities.
-     * @param targetUser the target user to impersonate
-     * @return a list of merged granted authorities
-     */
-    private List<GrantedAuthority> getMergedGrantedAuthorities(UserDetails targetUser) {
-        Authentication currentAuthentication = this.securityContextHolderStrategy.getContext().getAuthentication();
-        if (currentAuthentication == null) {
-            throw new RuntimeException("Trying to impersonate without an existing authentication");
-        }
-        Collection<? extends GrantedAuthority> targetUserAuthorities = targetUser.getAuthorities();
-        List<GrantedAuthority> switchUserAuthoritiesList = new ArrayList<>(targetUserAuthorities);
-        switchUserAuthoritiesList.addAll(getOriginalAuthorities(currentAuthentication));
-        return switchUserAuthoritiesList;
-    }
-
-    private Collection<GrantedAuthority> getOriginalAuthorities(Authentication currentAuthentication) {
-//        List<GrantedAuthority> currentAuthoritiesList = new ArrayList<>();
-//        for (GrantedAuthority authority : currentAuthentication.getAuthorities()) {
-//            GrantedAuthority currentUserGrantedAuthority = new SwitchUserGrantedAuthority(authority.getAuthority(),
-//                    currentAuthentication);
-//            currentAuthoritiesList.add(currentUserGrantedAuthority);
-//        }
-//        return currentAuthoritiesList;
-
-        // by default this adds some non-existing role,
-        // so that the original authorities are not taken into account, i.e.
-        // a user cannot act as admin after impersonation.
-        // This however can be changed by adding all current authorities.
-        return Collections.singleton(new SwitchUserGrantedAuthority(
-                "ROLE_PREVIOUS_ADMINISTRATOR", currentAuthentication));
-    }
-
-    private Optional<Authentication> getOriginalAuthentication(Authentication currentAuthentication) {
-        // iterate over granted authorities and find the 'switch user' authentications
-        Collection<? extends GrantedAuthority> authenticationAuthorities = currentAuthentication.getAuthorities();
-        for (GrantedAuthority grantedAuthority : authenticationAuthorities) {
-            // return original Authentication that is mapped to any SwitchUserGrantedAuthority
-            // (expected that all SwitchUserGrantedAuthority are mapped to a single Authentication)
-            if (grantedAuthority instanceof SwitchUserGrantedAuthority) {
-                return Optional.of(((SwitchUserGrantedAuthority) grantedAuthority).getSource());
-            }
-        }
-        return Optional.empty();
-    }
 
     private void updateUserName(UserDetails targetUser) {
         if (targetUser != null) {
